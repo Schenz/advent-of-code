276b02a633819275805fb4cf80743326b5f6c133
schenz <bschenz@gmail.com>
2023-12-02 16:50:42 -0500

    2022 day09

--- test/2022/day09.test.ts
+++ test/2022/day09.test.ts
@@ -1,15 +1,29 @@
 // Advent of Code - Day 9
 
-import { part1, part2 } from '../src/day09';
+import { part1, part2 } from '../../src/2022/day09';
 
 describe('part one tests', () => {
   it('part one test', () => {
-    expect(part1([""])).toBe(0);
+    expect(part1(["R 4",
+    "U 4",
+    "L 3",
+    "D 1",
+    "R 4",
+    "D 1",
+    "L 5",
+    "R 2"])).toBe(13);
   });
 });
 
 describe('part two tests', () => {
   it('part two test', () => {
-    expect(part2([""])).toBe(0);
+    expect(part2(["R 5",
+    "U 8",
+    "L 8",
+    "D 3",
+    "R 17",
+    "D 10",
+    "L 25",
+    "U 20"])).toBe(36);
   });
 });
