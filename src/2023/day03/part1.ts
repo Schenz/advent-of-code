fc1a58a5866f0db324bc5ee42eceb6e0362175b2
schenz <bschenz@gmail.com>
2023-12-03 01:37:19 -0500

    2023 day 3

---
+++ src/2023/day03/part1.ts
@@ -0,0 +1,39 @@
+// Advent of Code - Day 3 - Part One
+
+export function getAdj(y: number, x: number): [number, number][] {
+  return [
+      [y - 1, x - 1], [y - 1, x], [y - 1, x + 1],
+      [y, x - 1], [y, x + 1],
+      [y + 1, x - 1], [y + 1, x], [y + 1, x + 1],
+  ]
+}
+
+export function part1(lines: string[]): number {
+  const ok = (c: string) => (c >= '0' && c <= '9') || c === '.'
+
+    let vs = []
+    for (let y = 0; y < lines.length; y++) {
+        const line = lines[y]
+        let seq = ''
+        let bad = false
+        for (let x = 0; x < line.length; x++) {
+            const char = line[x]
+            if (char >= '0' && char <= '9') {
+                if (!getAdj(y, x).map(([ay, ax]) => lines.at(ay)?.at(ax)).every(a => a === undefined || ok(a))) {
+                    bad = true
+                }
+                seq += char
+            } else {
+                if (bad && seq.length > 0) {
+                    vs.push(parseInt(seq))
+                }
+                seq = ''
+                bad = false
+            }
+        }
+        if (bad && seq.length > 0) {
+            vs.push(parseInt(seq))
+        }
+    }
+    return vs.reduce((a, b) => a + b, 0)
+}
