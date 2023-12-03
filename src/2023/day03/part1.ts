7fc0d096a9c441a676460c3d6f752c1ea9f7ad60
schenz <bschenz@gmail.com>
2023-12-03 10:59:21 -0500

    refactor for readability

--- src/2023/day03/part1.ts
+++ src/2023/day03/part1.ts
@@ -15,23 +15,23 @@ export function part1(lines: string[]): number {
   for (let y = 0; y < lines.length; y++) {
     const line = lines[y]
     let seq = ''
-    let bad = false
+    let isPart = false
     for (let x = 0; x < line.length; x++) {
       const char = line[x]
       if (char >= '0' && char <= '9') {
         if (!getAdj(y, x).map(([ay, ax]) => lines.at(ay)?.at(ax)).every(a => a === undefined || ok(a))) {
-          bad = true
+          isPart = true
         }
         seq += char
       } else {
-        if (bad && seq.length > 0) {
+        if (isPart && seq.length > 0) {
           parts.push(parseInt(seq))
         }
         seq = ''
-        bad = false
+        isPart = false
       }
     }
-    if (bad && seq.length > 0) {
+    if (isPart && seq.length > 0) {
       parts.push(parseInt(seq))
     }
   }
