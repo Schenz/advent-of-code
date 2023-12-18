6e70365e51fd0c2f8b719b78f23e27a854db72b8
schenz <bschenz@gmail.com>
2023-12-18 10:13:28 -0500

    add context and tests

--- test/2023/day18.test.ts
+++ test/2023/day18.test.ts
@@ -1,6 +1,7 @@
 // Advent of Code - Day 18
 
 import { part1, part2 } from '../../src/2023/day18';
+import { readFile } from 'fs/promises';
 
 const testData = [
     'R 6 (#70c710)',
@@ -23,10 +24,27 @@ describe('part one tests', () => {
     it('part one test', () => {
         expect(part1(testData)).toBe(62);
     });
+
+    it('part one test - real data', async () => {
+        expect(part1(await getData())).toBe(40131);
+    });
 });
 
 describe('part two tests', () => {
     it('part two test', () => {
         expect(part2(testData)).toBe(952408144115);
     });
+
+    it('part two test - real data', async () => {
+        expect(part2(await getData())).toBe(104454050898331);
+    });
 });
+
+const getData = async (): Promise<string[]> => {
+    const input: string = await readFile(
+        'src/2023/day18/resources/input.txt',
+        'utf8'
+    );
+
+    return input.trim().split(/\r?\n/);
+};
