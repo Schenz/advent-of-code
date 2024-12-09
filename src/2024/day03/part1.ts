c416cc357d6bb2ccf0e93e739154c9975f22e2a4
Brandon Schenz <bschenz@gmail.com>
2024-12-09 15:46:36 -0500

    fix linting issues

--- src/2024/day03/part1.ts
+++ src/2024/day03/part1.ts
@@ -1,11 +1,19 @@
 // Advent of Code - Day 3 - Part One
 
-export const part1 = (input: string[]): number => input.reduce((sum, line) => {
-    const matches = line.match(/mul\(\d+,\d+\)/g);
-    if (!matches) return sum;
+export const part1 = (input: string[]): number =>
+    input.reduce((sum, line) => {
+        const matches = line.match(/mul\(\d+,\d+\)/g);
 
-    return sum + matches.reduce((acc, match) => {
-        const [a, b] = match.slice(4, -1).split(',').map(Number);
-        return acc + (a * b);
-    }, 0);;
-}, 0);
+        if (!matches) {
+            return sum;
+        }
+
+        return (
+            sum +
+            matches.reduce((acc, match) => {
+                const [a, b] = match.slice(4, -1).split(',').map(Number);
+
+                return acc + a * b;
+            }, 0)
+        );
+    }, 0);
--- src/2024/day03/part2.ts
+++ src/2024/day03/part2.ts
@@ -5,9 +5,9 @@ export const part2 = (input: string[]): number => {
 
     return input.reduce((sum, line) => {
         const parts = line.split(/(do\(\)|don't\(\))/);
-        
+
         return parts.reduce((partSum, part) => {
-            if (part === "do()") {
+            if (part === 'do()') {
                 mulEnabled = true;
                 return partSum;
             } else if (part === "don't()") {
@@ -15,15 +15,24 @@ export const part2 = (input: string[]): number => {
                 return partSum;
             }
 
-            if (!mulEnabled) return partSum;
+            if (!mulEnabled) {
+                return partSum;
+            }
 
             const matches = part.match(/mul\(\d+,\d+\)/g);
-            if (!matches) return partSum;
 
-            return partSum + matches.reduce((acc, match) => {
-                const [a, b] = match.slice(4, -1).split(',').map(Number);
-                return acc + (a * b);
-            }, 0);;
+            if (!matches) {
+                return partSum;
+            }
+
+            return (
+                partSum +
+                matches.reduce((acc, match) => {
+                    const [a, b] = match.slice(4, -1).split(',').map(Number);
+
+                    return acc + a * b;
+                }, 0)
+            );
         }, sum);
     }, 0);
 };
