7fbc81ce06a703067e50874ab6e5377615d5f68a
Brandon Schenz <bschenz@gmail.com>
2024-12-09 15:45:40 -0500

    fix linting issues

--- src/2024/day01/part1.ts
+++ src/2024/day01/part1.ts
@@ -4,14 +4,18 @@ export const part1 = (input: string[]): number => {
     const list1: number[] = [];
     const list2: number[] = [];
 
-    input.forEach(line => {
+    input.forEach((line) => {
         const [num1, num2] = line.trim().split(/\s+/).map(Number);
+
         list1.push(num1);
         list2.push(num2);
     });
 
     list1.sort((a, b) => a - b);
     list2.sort((a, b) => a - b);
-    
-    return list1.reduce((sum, num, index) => sum + Math.abs(num - list2[index]), 0);
+
+    return list1.reduce(
+        (sum, num, index) => sum + Math.abs(num - list2[index]),
+        0
+    );
 };
--- src/2024/day01/part2.ts
+++ src/2024/day01/part2.ts
@@ -4,15 +4,18 @@ export const part2 = (input: string[]): number => {
     const list1: number[] = [];
     const countMap = new Map<number, number>();
 
-    input.forEach(line => {
+    input.forEach((line) => {
         const [num1, num2] = line.trim().split(/\s+/).map(Number);
+
         list1.push(num1);
         countMap.set(num2, (countMap.get(num2) || 0) + 1);
     });
 
     return list1.reduce((similarityScore, num) => {
-        if (countMap.has(num)) {
-            similarityScore += num * countMap.get(num)!;
+        const count = countMap.get(num);
+
+        if (count !== undefined) {
+            similarityScore += num * count;
         }
         return similarityScore;
     }, 0);
