d408ca3dcf15e25d28a0cdaadbcb2bbd1351c264
Brandon Schenz <bschenz@gmail.com>
2025-11-16 00:54:24 -0500

    Full ESM Migration

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,5 +1,9 @@
 export default {
+    extensionsToTreatAsEsm: ['.ts'],
     transform: {
         '^.+\\.(t|j)sx?$': ['@swc/jest'],
+    },
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
     },
 };
