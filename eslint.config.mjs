8cc3aecebcbe2cd1c64c414ae763fd0c2d532191
Brandon Schenz <bschenz@gmail.com>
2025-11-16 00:44:43 -0500

    modernize project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,7 @@ import globals from 'globals';
 export default [
     // Ignore patterns (replacing .eslintignore)
     {
-        ignores: ['node_modules/**', 'build/**', 'coverage/**', 'dist/**', 'jest.config.js'],
+        ignores: ['node_modules/**', 'build/**', 'coverage/**', 'dist/**'],
     },
 
     // Base recommended configs
--- jest.config.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-module.exports = {
-    transform: {
-        '^.+\\.(t|j)sx?$': ['@swc/jest'],
-    },
-};
